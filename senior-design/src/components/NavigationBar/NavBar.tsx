5cff29f611d3c240eaf96e6825096f1c4f2d2f51
svyatoslavvarnitskyy <svarnits@iastate.edu>
2024-11-11 17:41:50 -0600

    Updated toolbar

--- senior-design/src/components/NavigationBar/NavBar.tsx
+++ senior-design/src/components/NavigationBar/NavBar.tsx
@@ -1,16 +1,10 @@
-import * as React from 'react';
-import { alpha, styled } from '@mui/material/styles';
+import {alpha, styled} from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
 import Container from '@mui/material/Container';
-import Divider from '@mui/material/Divider';
-import MenuItem from '@mui/material/MenuItem';
-import Drawer from '@mui/material/Drawer';
-import MenuIcon from '@mui/icons-material/Menu';
-import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
+import {Build, Info, Search, Settings, Undo} from "@mui/icons-material";
 
 const StyledToolbar = styled(Toolbar)(({ theme }) => ({
     display: 'flex',
@@ -28,15 +22,14 @@ const StyledToolbar = styled(Toolbar)(({ theme }) => ({
 
 const StyledButton = styled(Button)(() => ({
     marginRight: '8px',
+    backgroundColor: 'darkgray',
+    textTransform: 'none',
+    maxHeight: '120%',
+    fontFamily: 'monospace'
 }))
 
 
 export const NavBar = () => {
-    const [open, setOpen] = React.useState(false);
-
-    const toggleDrawer = (newOpen: boolean) => () => {
-        setOpen(newOpen);
-    };
 
     return (
         <AppBar
@@ -51,86 +44,27 @@ export const NavBar = () => {
         >
             <Container maxWidth="lg">
                 <StyledToolbar variant="dense" disableGutters>
-                    <Box sx={{ flexGrow: 1, display: 'flex', alignItems: 'center', px: 0 }}>
+                    <Box sx={{ flexGrow: 1, display: 'flex', alignItems: 'center', px: 0, justifyContent: 'left' }}>
                         <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-                            <StyledButton variant="contained" size="small">
-                                Features
+                            <StyledButton variant="contained" size="small" startIcon={<Search/>}>
+                                Query
                             </StyledButton>
-                            <StyledButton variant="outlined" size="small">
-                                Testimonials
+                            <StyledButton variant="contained" size="small" startIcon={<Undo/>}>
+                                History
                             </StyledButton>
-                            <StyledButton variant="outlined" size="small">
-                                Highlights
+                            <StyledButton variant="contained" size="small" startIcon={<Build/>}>
+                                Utilities
                             </StyledButton>
-                            <StyledButton variant="outlined" size="small">
-                                Pricing
+                            <StyledButton variant="contained" size="small" startIcon={<Settings/>}>
+                                Settings
                             </StyledButton>
-                            <StyledButton variant="outlined" size="small" sx={{ minWidth: 0 }}>
-                                FAQ
-                            </StyledButton>
-                            <StyledButton variant="outlined" size="small" sx={{ minWidth: 0 }}>
-                                Blog
+                            <StyledButton variant="contained" size="small" startIcon={<Info/>}>
+                                About Us
                             </StyledButton>
                         </Box>
                     </Box>
-                    <Box
-                        sx={{
-                            display: { xs: 'none', md: 'flex' },
-                            gap: 1,
-                            alignItems: 'center',
-                        }}
-                    >
-                        <Button color="primary" variant="text" size="small">
-                            Sign in
-                        </Button>
-                        <Button color="primary" variant="contained" size="small">
-                            Sign up
-                        </Button>
-                    </Box>
-                    <Box sx={{ display: { xs: 'flex', md: 'none' }, gap: 1 }}>
-                        <IconButton aria-label="Menu button" onClick={toggleDrawer(true)}>
-                            <MenuIcon />
-                        </IconButton>
-                        <Drawer
-                            anchor="top"
-                            open={open}
-                            onClose={toggleDrawer(false)}
-                            PaperProps={{
-                                sx: {
-                                    top: 'var(--template-frame-height, 0px)',
-                                },
-                            }}
-                        >
-                            <Box sx={{ p: 2, backgroundColor: 'background.default' }}>
-                                <Box
-                                    sx={{
-                                        display: 'flex',
-                                        justifyContent: 'flex-end',
-                                    }}
-                                >
-                                    <IconButton onClick={toggleDrawer(false)}>
-                                        <CloseRoundedIcon />
-                                    </IconButton>
-                                </Box>
-                                <MenuItem>Features</MenuItem>
-                                <MenuItem>Testimonials</MenuItem>
-                                <MenuItem>Highlights</MenuItem>
-                                <MenuItem>Pricing</MenuItem>
-                                <MenuItem>FAQ</MenuItem>
-                                <MenuItem>Blog</MenuItem>
-                                <Divider sx={{ my: 3 }} />
-                                <MenuItem>
-                                    <Button color="primary" variant="contained" fullWidth>
-                                        Sign up
-                                    </Button>
-                                </MenuItem>
-                                <MenuItem>
-                                    <Button color="primary" variant="outlined" fullWidth>
-                                        Sign in
-                                    </Button>
-                                </MenuItem>
-                            </Box>
-                        </Drawer>
+                    <Box component='section' sx={{ p: 1, border: '5px dashed black', fontSize: '20px', fontFamily:'monospace' }}>
+                        HADES
                     </Box>
                 </StyledToolbar>
             </Container>
