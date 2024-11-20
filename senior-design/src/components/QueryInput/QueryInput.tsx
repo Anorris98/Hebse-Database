fb153e1255c7a4625b9aff02e9cfec5855627982
snyder10 <snyder10@iastate.edu>
2024-11-20 00:07:30 -0600

    feat: implemented local query saving alongside dropdown for accessing saved queries and deleting them

--- senior-design/src/components/QueryInput/QueryInput.tsx
+++ senior-design/src/components/QueryInput/QueryInput.tsx
@@ -1,11 +1,20 @@
 import Box from "@mui/material/Box";
-import {Grid2, IconButton, InputAdornment, TextField} from "@mui/material";
-import {AutoAwesome} from "@mui/icons-material";
-import { useState } from 'react';
+import { Grid2, IconButton, InputAdornment, TextField } from "@mui/material";
+import { ArrowDropDown, AutoAwesome, Clear, ContentCopyOutlined, Save } from "@mui/icons-material";
+import { useEffect, useState } from 'react';
 
 
 export const QueryInput = () => {
     const [inputValue, setInputValue] = useState('');
+    const [savedQueries, setSavedQueries] = useState<string[]>([]);
+    const [displaySavedQueries, setDisplaySavedQueries] = useState<boolean>(false);
+
+    useEffect(() => {
+        if ("saved" in localStorage) {
+            setSavedQueries(JSON.parse(localStorage.saved));
+        }
+    }, []);
+
     async function getSQLFromNaturalLanguage() {
         let data = {
             "string": inputValue
@@ -17,6 +26,25 @@ export const QueryInput = () => {
                 "Content-Type": "application/json"
             }
         }).then(response => response.json()).then(body => setInputValue(body.message));
+    }
+
+    function saveQuery() {
+        let saved = [...savedQueries];
+        if (inputValue != '' && !saved.includes(inputValue)) {
+            saved.push(inputValue);
+            localStorage.saved = JSON.stringify(saved);
+            setSavedQueries(saved);
+        }
+    }
+
+    function deleteQuery(item: string) {
+        let saved = [...savedQueries];
+        const index = saved.indexOf(item);
+        if (index > -1) {
+            saved.splice(index, 1);
+            localStorage.saved = JSON.stringify(saved);
+            setSavedQueries(saved);
+        }
     }
 
     return (
@@ -46,6 +74,48 @@ export const QueryInput = () => {
                 </Box>
             </Box>
             <Box component="div" sx={{
+            alignItems: 'center',
+            flexGrow: 1,
+            maxWidth: 'lg',
+            backgroundColor: 'gray',
+            borderRadius: '15px',
+            marginTop: '20px',
+            fontFamily: 'monospace'
+            }}>
+                <Box sx={{fontSize: '20px', fontWeight: 'bold'}}>
+                    Saved Queries <IconButton sx={{maxHeight:"10px", width: "30px"}}  onClick={() => setDisplaySavedQueries(!displaySavedQueries)}><ArrowDropDown
+                                sx={{color: 'white', backgroundColor:"black"}}/></IconButton>
+                </Box>
+                {
+                    displaySavedQueries ?
+                    savedQueries.map((item: string, index: number) => (
+                        <Box sx={{
+                            borderRadius: "8px",
+                            border: "1px solid transparent",
+                            textAlign:'center',
+                            maxWidth: "calc(100% - 24px)",
+                            padding: '4px',
+                            margin: '8px',
+                            backgroundColor: 'darkgray',
+                            textTransform: 'none',
+                            fontFamily: 'monospace',
+                            fontSize:"16px",
+                            color: 'white',
+                            display:"inline-flex",
+                            alignItems: "center",
+                            flexDirection: 'row-reverse'
+                            }} key={index}>
+                            <IconButton sx={{height: "10px", width: "30px", textAlign: "center"}} onClick={() => deleteQuery(item)}><Clear
+                                sx={{color: 'white', backgroundColor:"black"}}/></IconButton>
+                            <IconButton sx={{height: "10px", width: "30px", textAlign: "center"}} onClick={() => setInputValue(item)}><ContentCopyOutlined
+                                sx={{color: 'white', backgroundColor:"black"}}/></IconButton>
+                            <Box flexGrow={1} component="div" sx={{whiteSpace: 'normal', overflowWrap: 'break-word', textAlign:'center', width:"calc(100% - 60px)"}}>{item}</Box>
+                        </Box>
+                    )) : null
+                }
+
+            </Box>
+            <Box component="div" sx={{
                 backgroundColor: 'gray',
                 flexGrow: 1,
                 borderRadius: '15px',
@@ -58,6 +128,8 @@ export const QueryInput = () => {
                         input: {
                             style: {fontFamily: 'monospace', color: 'white'},
                             startAdornment: <InputAdornment position={"start"}> <IconButton onClick={() => getSQLFromNaturalLanguage()}><AutoAwesome
+                                sx={{color: 'white'}}/></IconButton></InputAdornment>,
+                            endAdornment: <InputAdornment position={"end"}> <IconButton onClick={() => saveQuery()}><Save
                                 sx={{color: 'white'}}/></IconButton></InputAdornment>
                         }, inputLabel: {style: {fontFamily: 'monospace', color: 'white'}}
                     }} sx={{margin: 1}}/>
