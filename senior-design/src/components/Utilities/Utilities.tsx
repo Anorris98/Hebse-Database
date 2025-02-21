19cc31a1366a9eef3c3b3940abe10dd80201eed1
anorris <anorris@iastate.edu>
2025-02-20 21:51:48 -0600

    fixed weird issue with nav bar double rendering and becoming a weird off white color on the utilities page. Fix was to remove the navbar from the component

--- senior-design/src/components/Utilities/Utilities.tsx
+++ senior-design/src/components/Utilities/Utilities.tsx
@@ -1,10 +1,8 @@
-import { NavBar } from "../NavigationBar/NavBar.tsx";
 import { Grid2 } from "@mui/material";
 
 export const Utilities = () => {
     return (
         <div>
-            <NavBar /> {/* The navigation bar */}
             <Grid2 display={"grid"} sx={{ maxHeight: '1000px' }}>
                 {/* Components for settings and utilities will be added here */}
             </Grid2>
