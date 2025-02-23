80572e35f6890375238f5de0261ac561baaa6d7e
Alex Polston <apolston@iastate.edu>
2025-02-23 10:03:49 -0600

    Started creating tox test framework

---
+++ backend/tests/test_main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import create_engine, text
+import os
+import sshtunnel
+
+def test_get_data():
+        return get_data() =  #Need sql statement inside parenthesis
