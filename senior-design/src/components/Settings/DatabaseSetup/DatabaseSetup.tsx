ce353dd2b537b5005810c63fc19a0dbeca4d93f6
anorris <anorris@iastate.edu>
2025-03-02 19:01:09 -0600

    styling update

--- senior-design/src/components/Settings/DatabaseSetup/DatabaseSetup.tsx
+++ senior-design/src/components/Settings/DatabaseSetup/DatabaseSetup.tsx
@@ -1,16 +1,5 @@
 import { useState, useEffect } from "react";
-import {
-  Box,
-  Button,
-  Typography,
-  Container,
-  IconButton,
-  InputAdornment,
-  MenuItem,
-  Paper,
-  Switch,
-  FormControlLabel
-} from "@mui/material";
+import {Box, Button, Typography, Container, IconButton, InputAdornment, MenuItem, Paper, Switch, FormControlLabel} from "@mui/material";
 import { alpha, styled } from "@mui/material/styles";
 import {
   Visibility,
