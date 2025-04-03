d262dc1a72ffe91ed5914fc24b8b78e42cc0ff34
jwbyrd <jwbyrd@iastate.edu>
2025-04-03 14:49:41 -0500

    font style fix

--- senior-design/src/components/Utilities/DataDownload/data-download.tsx
+++ senior-design/src/components/Utilities/DataDownload/data-download.tsx
@@ -102,6 +102,7 @@ const DatasetList: React.FC = () => {
                   sx={{ 
                     marginBottom: "12px", 
                     color: "white", 
+                    fontFamily: "monospace"
                   }}>
                   {dataset.title}
                 </Typography>
