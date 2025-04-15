1fd9f9fc93f0918c0075c6e23b61fbc9350ac114
svyatoslavvarnitskyy <svarnits@iastate.edu>
2025-04-15 14:36:44 -0500

    Fixed conflict

--- senior-design/src/app/routes.tsx
+++ senior-design/src/app/routes.tsx
@@ -5,17 +5,59 @@ import {Utilities} from "../components/Utilities/utilities.tsx";
 import {AboutUs} from "../components/About_Us/about-us.tsx";
 import {Settings} from "../components/Settings/settings.tsx";
 import {History} from "../components/History/history.tsx";
+import {createTheme, ThemeProvider} from "@mui/material";
 /* istanbul ignore file -- @preserve */
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#ffffff",
+        },
+        secondary: {
+            main: "#ffffff",
+        },
+        background: {
+            default: "#ffffff",
+        },
+        text: {
+            primary: "#ffffff",
+        },
+    },
+    typography: {
+        fontFamily: "monospace",
+        h1: {
+            fontSize: "2.5rem",
+            fontWeight: 600,
+        },
+        button: {
+            textTransform: "none", // Prevent uppercase transformation
+        },
+    },
+    components: {
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    borderRadius: "12px", // Custom border radius for buttons
+                },
+            },
+        },
+    },
+});
+
+
 const App = () => {
     return (
-        <Routes>
-            <Route path='/' element={<Query/>}/>
-            <Route path='/Settings' element={<Settings/>}/>
-            <Route path='/History' element={<History/>}/>
-            <Route path='/Utilities' element={<Utilities/>}/>
-            <Route path='/About_Us' element={<AboutUs/>}/>
-            <Route path='*' element={<ErrorPage/>}/>
-        </Routes>
+        <ThemeProvider theme={theme}>
+            <Routes>
+                <Route path='/' element={<Query/>}/>
+                <Route path='/Settings' element={<Settings/>}/>
+                <Route path='/History' element={<History/>}/>
+                <Route path='/Utilities' element={<Utilities/>}/>
+                <Route path='/About_Us' element={<AboutUs/>}/>
+                <Route path='*' element={<ErrorPage/>}/>
+            </Routes>
+        </ThemeProvider>
+
     )
 };
 
