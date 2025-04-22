aa1179bc3401043034e6d0e87fd25ae3cf45617c
Alex Polston <apolston@iastate.edu>
2025-04-22 15:38:36 -0500

    removed 'alpha' from datadownload

--- senior-design/src/components/Utilities/DataDownload/data-download.tsx
+++ senior-design/src/components/Utilities/DataDownload/data-download.tsx
@@ -5,7 +5,7 @@ import ExpandLessIcon from "@mui/icons-material/ExpandLess";
 import DownloadIcon from "@mui/icons-material/Download";
 import UploadFile from "@mui/icons-material/UploadFile";
 import {useEffect, useState} from 'react';
-import {alpha, styled} from "@mui/material/styles";
+import {styled} from "@mui/material/styles";
 /* istanbul ignore file -- @preserve */
 // Styled components
 const StyledPaper = styled(Paper)(({ theme }) => ({
